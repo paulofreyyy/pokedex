11fb2eeff40317ee7b6cb5817832857572cfc031
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-01-24 16:48:52 -0300

    feat: finalizando componente EvolutionChain

--- src/components/evolution_chain/index.tsx
+++ src/components/evolution_chain/index.tsx
@@ -5,62 +5,54 @@ interface Props {
 }
 
 export const EvolutionChain = ({ pokemonDetails }: Props) => {
+    // Se a cadeia de evolução tiver apenas um item, não renderiza o componente
+    if (pokemonDetails.evolution_chain.length <= 1) {
+        return null;
+    }
+
     return (
         <>
             <Box
                 display='flex'
                 alignItems='center'
-                justifyContent='space-evenly'
+                justifyContent='center'
             >
-                <Box
-                    component="img"
-                    src={pokemonDetails.evolution_chain[0].image}
-                    alt={pokemonDetails.name}
-                    sx={{
-                        height: 100,
-                        borderRadius: 5,
-                        zIndex: 1,
-                    }}
-                />
+                {pokemonDetails.evolution_chain.map((evolution: any, index: any) => (
+                    <Box
+                        key={evolution.number}
+                        display='flex'
+                        alignItems='center'
+                    >
+                        {/* Imagem do Pokémon */}
+                        <Box
+                            component="img"
+                            src={evolution.image}
+                            alt={evolution.name}
+                            sx={{
+                                height: 100,
+                                borderRadius: 5,
+                                zIndex: 1,
+                                mx: 1,
+                            }}
+                        />
 
-                <Chip
-                    label={`Lvl ${pokemonDetails.evolution_chain[1].min_level}`}
-                    size="small"
-                    sx={{
-                        fontWeight: 600,
-                        px: 1
-                    }}
-                />
+                        {/* Chip com nível mínimo, exceto no último item */}
+                        {index < pokemonDetails.evolution_chain.length - 1 && (
+                            <Chip
+                                label={`Lvl ${pokemonDetails.evolution_chain[index + 1].min_level || "???"}`}
+                                size="small"
+                                sx={{
+                                    fontWeight: 600,
+                                    px: 1,
+                                    mx: 1,
+                                }}
+                            />
+                        )}
 
-                <Box
-                    component="img"
-                    src={pokemonDetails.evolution_chain[1].image}
-                    alt={pokemonDetails.name}
-                    sx={{
-                        height: 100,
-                        borderRadius: 5,
-                        zIndex: 1,
-                    }}
-                />
-                <Chip
-                    label={`Lvl ${pokemonDetails.evolution_chain[2].min_level}`}
-                    size="small"
-                    sx={{
-                        fontWeight: 600,
-                        px: 1
-                    }}
-                />
+                    </Box>
 
-                <Box
-                    component="img"
-                    src={pokemonDetails.evolution_chain[2].image}
-                    alt={pokemonDetails.name}
-                    sx={{
-                        height: 100,
-                        borderRadius: 5,
-                        zIndex: 1,
-                    }}
-                />
+
+                ))}
             </Box>
         </>
     )
